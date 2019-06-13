9fc4fe3fc34b34911b3f52f870a93bdf3f649fbf
Aleksi Huotala <aleksi.huotala@reaktor.com>
2019-06-13 14:37:03 +0300

    Move Provider to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import "./App.scss";
-import { Provider } from "mobx-react";
-import { noteStore } from "./store/NoteStore";
 import { Notes } from "./components/Notes";
 import { AddNoteForm } from "./components/AddNoteForm";
 
 const App: React.FC = () => (
-  <Provider noteStore={noteStore}>
     <>
       <AddNoteForm />
       <Notes />
     </>
-  </Provider>
 );
 
 export default App;
